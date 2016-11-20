2cf2b8414dc0108866721d9047159c513e50f3de
sn2015 <renew4@yandex.ru>
2016-11-20 19:48:07 +0300

    some changes

--- load.R
+++ load.R
@@ -1,7 +1,7 @@
 ### here i download data from the internet 
 ### then load data into R 
 ### and create two character files in working directory: training set 60% and a test set 40% of data
-# files have structure like this 
+# final files have structure like this 
 # trainset[1:5]
 # [1] "How are you? Btw thanks for the RT. You gonna be in DC anytime soon? Love to see you. Been way, way too long."
 # [2] "i no! i get another day off from skool due to the wonderful snow (: and THIS wakes me up...damn thing"        
@@ -12,7 +12,7 @@
 setwd("C:/Users/Sergey/Documents/Coursera/DataScientist/CapstoneProject/CapstoneDataset")
 
 ## downloading data from the internet 
-## Url was provided by Coursera, also i can use any other datasets
+## Url was provided by Coursera
 
 Url <- "https://d396qusza40orc.cloudfront.net/dsscapstone/dataset/Coursera-SwiftKey.zip"
 download.file(Url, "Coursera-SwiftKey.zip")
@@ -20,10 +20,9 @@ datedownloaded <- date() # Sat Jun 04 15:20:50 2016
 
 unzip("Coursera-SwiftKey.zip")
 
-list.files()
-list.files("final/")
-list.files("final/en_US")
-
+# list.files()
+# list.files("final/")
+# list.files("final/en_US")
 
 ## reading data into R
 
@@ -40,16 +39,16 @@ USblogs <- readLines(con, skipNul = TRUE, warn = FALSE)
 close(con)
 
 ## combaining datasets together
-sample <- c(UStwitter, USblogs, USnews)
+datasets <- c(UStwitter, USblogs, USnews)
 rm(con, UStwitter, USblogs, USnews)
 
 ## creating trainset and testset
 set.seed(1234)
-sampleline <- rbinom(length(sample), 1, 0.60)
-trainset <- sample[as.logical(sampleline)]
-testset <- sample[!as.logical(sampleline)]
+sampleline <- rbinom(length(datasets), 1, 0.60)
+trainset <- datasets[as.logical(sampleline)]
+testset <- datasets[!as.logical(sampleline)]
 
-rm(sampleline, sample)
+rm(sampleline, datasets)
 
 write(trainset, file = "trainset.txt")
 write(testset, file = "testset.txt")
