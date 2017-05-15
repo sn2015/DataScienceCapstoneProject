77005d2e312706493991e194337ed91ce58cf904
sn2015 <renew4@yandex.ru>
2017-05-15 11:04:40 +0300

    add new model
    which use input sentencet filter unigram, bigram and trigram and merge
    them to get a final table with probabilities of next word

--- model.R
+++ model.R
@@ -1,34 +1,106 @@
-## this script should create a function for predict next word
-## this function should take a sentence (givenwords) as input and a word as output
-## this fuction use "clean" function to input sentence so we can use ngrams which were bild 
-## with the same algorithm used
-## object trigram and givenword should be in working environment
-
-
-model <- function(x) {
-
-setwd("C:/Users/Sergey/Documents/Coursera/DataScientist/CapstoneProject/CapstoneDataset")
-
-if (!exists("trigram")) {trigram <- read.table("trigram.csv", stringsAsFactors = FALSE)}
-
 library(quanteda)
+library(dplyr)
 
+setwd("C:/Users/Sergey/Documents/Coursera/DataScientist/CapstoneProject/CapstoneDataset/knsmooth")
+list.files()
+rm(list = ls())
 
-# cleaning givenwords
+phrase <- "I like how the same people are in almost all of Adam Sandler's"
 source("clean.R")
-clean(givenwords)
-givenwords <- cleanedset
-rm(cleanedset)
+phrase <- clean(phrase)
+rm(cleanedset, clean)
+# phrase <- "ill be there for you id live and id die"
+# phrase <- "this is good thank for the"
 
-words <- character(length(givenwords))
 
-for (i in 1:length(givenwords)) {
-  tempphrase <- unlist(tokenize(givenwords[i]))
-  words[i] <- trigram[trigram$word3 == tempphrase[length(tempphrase) - 2] &
-                        trigram$word2 == tempphrase[length(tempphrase) - 1], ][1, 4]
+system.time({
+# model
+phrase <- unlist(strsplit(phrase, split = " "))
+length(phrase)
+phrase <- phrase[(length(phrase) - 3):(length(phrase) - 1)]
+
+
+# phrase = wordn + ... + word3 + word2 + word1 after cleaning 
+# our goal is to predict word1 or choose between different words1
+# we use trigam data frame to find word1
+# we filter trigram using previose words (word3 and word2) to find a set of word1
+# we assign conditional probability to all elements of word1
+
+
+# first element is a unigram continuation probabality
+# it does not depent on any other following data table
+# head(unigram)
+finalunigram <- select(unigram, -sum)
+# head(finalunigram)
+# user  system elapsed 
+# 0       0       0 
+
+# next element is a bigram continuation
+modelbigram <- bigram %>% filter(word2 == phrase[2]) %>% mutate(p2 = discount / sum(count)) %>% 
+        select( - c(discount))
+# head(modelbigram)
+# sum(modelbigram$count)
+
+# user  system elapsed 
+# 0.08    0.00    0.08 
+
+# to combine bigram and unigram we need to find lambdas
+# finding lambda for a unigram using a bigram
+modellambda1 <- lambda1 %>% filter(word2 == phrase[2]) %>% 
+        mutate(lambda = d / sum(modelbigram$count) * sumfollow) %>% select( - c(sumfollow))
+
+# user  system elapsed 
+# 0.12    0.00    0.13 
+
+# head(modellambda1)
+# here i combine unigram continuation probability dataframe with lambba dataframe
+# and multiply lambda and unigram continuation probability
+finalunigram <- merge(modellambda1, finalunigram, all = TRUE)
+finalunigram <- finalunigram %>% mutate(p1 = pconti * lambda) %>% 
+        select( - c(lambda, pconti))
+# head(finalunigram)
+
+# user  system elapsed 
+# 0.71    0.00    0.70 
+
+
+# here i combine unigram continuation probability multiplied by lambda with bigram probability 
+modelbigram <- modelbigram %>% select( - count)
+modelbigram  <- merge(modelbigram, finalunigram, all = TRUE)
+modelbigram$p2[is.na(modelbigram$p2)] <- 0
+modelbigram <- modelbigram %>% mutate(p2 = p2 + p1) %>% select( -c(p1)) %>% arrange(desc(p2))
+# head(modelbigram)
+
+})
+# user  system elapsed 
+# 8.00    0.05    8.05 
+
+# modeltrigram shows every trigram which has word2 and word3 the same as in phrase 
+modeltrigram <- trigram %>% filter(word3 == phrase[1] & word2 == phrase[2]) %>% 
+        mutate(p3 = discount / sum(count))
+# head(modeltrigram)
+
+## i assume that modelbigram is not empty
+## checking if trigram is not empty
+## if modeltrigram is empty i use modelbigram as a final table
+# modeltrigram <- modeltrigram[0, ]
+if (dim(modeltrigram)[1] == 0) {
+  final <- modelbigram      
+} else{
+        # modelbigramlambdas shows lambda for a bigram
+        modellambda2 <- lambda2 %>% filter(word3 == phrase[1] & word2 == phrase[2]) %>%
+                mutate(lambda2 = d / sum(modeltrigram$count) * sumfollow) %>% select(- c(sumfollow))
+        # head(modellambda2)
+        
+        # here i combine lambda with a bigram probability 
+        modelbigram <- merge(modellambda2, modelbigram, all = TRUE)
+        # head(modelbigram)
+        modelbigram <- modelbigram %>% mutate(p2 = p2 * lambda2) %>% select(- c(lambda2))
+        
+        
+        # here i combine trigram probability with a bigram continuation probability 
+        modeltrigram <- modeltrigram %>% select(- c(count, discount))
+        modeltrigram <- merge(modeltrigram, modelbigram, all = TRUE)
+        modeltrigram$p3[is.na(modeltrigram$p3)] <- 0
+        final <- modeltrigram %>% mutate(p3 = p3 + p2) %>% select(- c(p2)) %>% arrange(desc(p3))
 }
-rm(tempphrase)
-predictedwords <<- words
-rm(words)
-}
-
